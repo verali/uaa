44eab5d63db60989591c185d219731ef778ecb85
Brittany <brittany.johnson@ge.com>
2016-09-26 14:48:19 -0700

    Add unit tests which did not merge properly into branch.

--- server/src/test/java/org/cloudfoundry/identity/uaa/provider/saml/SamlIdentityProviderDefinitionTests.java
+++ server/src/test/java/org/cloudfoundry/identity/uaa/provider/saml/SamlIdentityProviderDefinitionTests.java
@@ -2,6 +2,8 @@ package org.cloudfoundry.identity.uaa.provider.saml;
 
 import org.apache.commons.httpclient.contrib.ssl.StrictSSLProtocolSocketFactory;
 import org.cloudfoundry.identity.uaa.provider.SamlIdentityProviderDefinition;
+import org.cloudfoundry.identity.uaa.util.JsonUtils;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -163,5 +165,27 @@ public class SamlIdentityProviderDefinitionTests {
         }
         def.setSocketFactoryClassName(StrictSSLProtocolSocketFactory.class.getName());
         assertEquals(StrictSSLProtocolSocketFactory.class.getName(), def.getSocketFactoryClassName());
+    }
+
+    @Test
+    public void test_that_serialized_json_does_not_contain_addNew() {
+        SamlIdentityProviderDefinition samlDefinition = new SamlIdentityProviderDefinition()
+                .setMetaDataLocation("mymetadata")
+                .setLinkText("Test SAML Provider");
+            samlDefinition.setAddNew(false);
+            samlDefinition.setEmailDomain(Arrays.asList("test.com", "test2.com"));
+        String config = JsonUtils.writeValueAsString(samlDefinition);
+        Assert.assertTrue(config.contains("\"addShadowUserOnLogin\":false"));
+        Assert.assertFalse(config.contains("addNew"));
+    }
+
+    @Test
+    public void test_that_json_containing_addNew_is_deserialized() {
+        String config = "{\"metaDataLocation\":\"mymetadata\",\"idpEntityAlias\":\"1D1DCE7769624FC0B8B8C11344EA67B8\"," 
+                + "\"zoneId\":\"apm\",\"nameID\":\"unspecified\",\"assertionConsumerIndex\":0," 
+                + "\"metadataTrustCheck\":false,\"showSamlLink\":false,\"socketFactoryClassName\":" 
+                + "\"\",\"linkText\":\"Login with GE SSO staging\",\"iconUrl\":null,\"addNew\":false}";
+        SamlIdentityProviderDefinition samlDefinition = JsonUtils.readValue(config, SamlIdentityProviderDefinition.class);
+        Assert.assertFalse(samlDefinition.isAddShadowUserOnLogin());
     }
 }
